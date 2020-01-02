fc6a0ec3dfe2503b4416d4b88b5bfbe2ea4b87ba
Michael Polívka <Michael.Polivka@365.pslib.cz>
2020-01-02 01:14:19 +0100

    Clean Repo

---
+++ Checkers/Models/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checkers.Models
+{
+    public class Board
+    {
+        private const int Lenght = 8;
+        private const int Width = 8;
+        private Piece[,] Storage;
+        public Board()
+        {
+            this.Storage = new Piece[Lenght, Width];
+        }
+    }
+}
---
+++ Checkers/Models/Piece.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checkers.Models
+{
+    public class Piece
+    {
+        public int Type { get; set; } // 1 - normal, 2 - Dáma
+        public int Color { get; set; } // 0 - černá, 1 = červená
+
+        public Piece( int type, int color)
+        {
+            Color = color;
+            Type = type;
+        }
+    }
+}
